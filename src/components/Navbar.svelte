71d59dd9c6278fb3b97fce460bd2d67034df56c1
Muhammad Surya Jayadiprana <50776952+suryamsj@users.noreply.github.com>
2022-04-29 23:49:49 +0700

    📖 Al-Qur'an Digital
    Update navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -15,7 +15,8 @@
             <a
                 class="nav-link active"
                 aria-current="page"
-                href="https://github.com/suryamsj/alquran"
+                href="https://github.com/suryamsj/al-quran-digital"
+                target="_blank"
                 ><svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="25"
